3cf7e7738acbfb887e29de1f22e79c9f217dc5fc
Victor <kushnir.victor@gmail.com>
2021-04-20 08:31:39 +0300

    Test fix.

--- Test.cpp
+++ Test.cpp
@@ -3,10 +3,10 @@
 #include "doctest.h"
 using namespace std;
 using namespace ariel;
-TEST_CASE("Able to open the file.") {
-  CHECK_NOTHROW(ifstream units_file{"units.txt"});
-}
+ifstream units_file{"units.txt"};
+
 TEST_CASE("Testing correct addition.") {
+  NumberWithUnits::read_units(units_file);
   NumberWithUnits cm1{1, "cm"};
   NumberWithUnits cm2{2, "cm"};
   NumberWithUnits cm5{5, "cm"};
@@ -14,38 +14,50 @@ TEST_CASE("Testing correct addition.") {
   NumberWithUnits mpcm1{1.01, "m"};
   NumberWithUnits cmpm1{101, "cm"};
   NumberWithUnits wrong{159, "cm"};
-  CHECK((cm1++) == cm2);
-  CHECK((cm1 += (cm2 * 2)) == cm5);
+  cm1++;
+  CHECK(cm1 == cm2);
+  cm1--;
+  cm1 += cm2 * 2;
+  CHECK(cm1 == cm5);
+  cm1 -= cm2 * 2;
   CHECK((m1 + cm1) == mpcm1);
   CHECK((cm1 + m1) == cmpm1);
   CHECK((cm1 + m1) != wrong);
 }
 TEST_CASE("Testing incorrect addition.") {
+  NumberWithUnits::read_units(units_file);
   NumberWithUnits min1{1, "min"};
   NumberWithUnits m1{1, "m"};
   CHECK_THROWS(m1 + min1);
   CHECK_THROWS(min1 + m1);
 }
 TEST_CASE("Testing correct subtraction.") {
+  NumberWithUnits::read_units(units_file);
   NumberWithUnits cm1{1, "cm"};
   NumberWithUnits cm0{0, "cm"};
   NumberWithUnits m1{1, "m"};
   NumberWithUnits mmcm1{0.99, "m"};
   NumberWithUnits cmmm1{-99, "cm"};
   NumberWithUnits wrong{159, "cm"};
-  CHECK((cm1--) == cm0);
-  CHECK((cm1 -= m1) == cmmm1);
+  cm1--;
+  CHECK(cm1 == cm0);
+  cm1++;
+  cm1 -= m1;
+  CHECK(cm1 == cmmm1);
+  cm1 += m1;
   CHECK((m1 - cm1) == mmcm1);
   CHECK((cm1 - m1) == cmmm1);
   CHECK((cm1 - m1) != wrong);
 }
 TEST_CASE("Testing incorrect subtraction.") {
+  NumberWithUnits::read_units(units_file);
   NumberWithUnits min1{1, "min"};
   NumberWithUnits m1{1, "m"};
   CHECK_THROWS(m1 - min1);
   CHECK_THROWS(min1 - m1);
 }
 TEST_CASE("Testing correct multiplication.") {
+  NumberWithUnits::read_units(units_file);
   NumberWithUnits m5{5, "m"};
   NumberWithUnits m45{45, "m"};
   NumberWithUnits m0{0, "m"};
@@ -58,6 +70,7 @@ TEST_CASE("Testing correct multiplication.") {
   CHECK((m5 * 179) != m45);
 }
 TEST_CASE("Testing inequality signs.") {
+  NumberWithUnits::read_units(units_file);
   NumberWithUnits m5{5, "m"};
   NumberWithUnits m45{45, "m"};
   NumberWithUnits ils5{5, "ILS"};
@@ -78,3 +91,4 @@ TEST_CASE("Testing inequality signs.") {
   CHECK_THROWS(operator<=(ils5, m45));
   CHECK_THROWS(operator<(ils5, m45));
 }
+
